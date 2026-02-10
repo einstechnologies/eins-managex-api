d886b021c543ab5503eb9e4000c0d273f773400a
EINS-SD-VRUSHALI <vrushali@eins.co.in>
2026-02-10 13:14:28 +0530

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace eins_managex_api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
